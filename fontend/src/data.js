570414d19320fa71e10cfdf5fb0dde5c0fc50fc8
Remy4657 <trongdatga@gmail.com>
2022-07-27 16:46:24 +0700

    create node server

---
+++ fontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products: [
+        {
+            name: "Áo dài tay",
+            slug: "ao-dai-tay",
+            category: "Shirt",
+            image: '/images/p1.jpg',
+            price: 100,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality shirt"
+        },
+        {
+            name: "adidas shirt",
+            slug: "adidas-shirt",
+            category: "Shirt",
+            image: '/images/p2.jpg',
+            price: 120,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 30,
+            description: "high quality shirt"
+        },
+        {
+            name: "Nike slimp pants",
+            slug: "nike-slim-pants",
+            category: "Pants",
+            image: '/images/p3.jpg',
+            price: 90,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: "high quality shirt"
+        },
+        {
+            name: "Puma Fit Pants",
+            slug: "puma-fit-pants",
+            category: "Shirt",
+            image: '/images/p4.jpg',
+            price: 220,
+            countInStock: 5,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality shirt"
+        }
+    ]
+}
+export default data
---
+++ fontend/src/screens/ProductScreen.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom"
+
+
+const ProductScreen = () => {
+    const params = useParams()
+    const { slug } = params
+    return (
+        <div>
+            {slug}
+        </div>
+    )
+}
+export default ProductScreen
